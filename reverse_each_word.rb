4ebde04e0e4c4a30f1c0727ef63e02d57a4c1fd2
David Fellows <dfellows12@gmail.com>
2019-06-03 18:49:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence)
-  my_sentence = sentence.split(" ")
+  my_sentence = sentence.split
   new_sentence = []
   my_sentence.each do |word|
     new_sentence.push(word.reverse)
   end
-  final_sentence = new_sentence.join
+  final_sentence = new_sentence.join(" ")
   final_sentence
 end
