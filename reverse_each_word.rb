28e836f458e051d103670e4e1df5aeba14dda1cc
David Fellows <dfellows12@gmail.com>
2019-06-03 19:00:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 def reverse_each_word(sentence)
   my_sentence = sentence.split
-  final_sentence = my_sentence.collect {|word| word.reverse}
-  end
-  final_sentence.join(" ")
+  my_sentence.collect {|word| word.reverse}
 end
 
